9f84b1d89b19c93399231534ac2a841ba2aaa1b0
Kartik Srivastava <kartiksrivastav765@gmail.com>
2026-02-02 19:59:41 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, pos;
+    int arr[1000];
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements: ", n);
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter position to delete: ");
+    scanf("%d", &pos);
+
+    for(int i = pos - 1; i < n - 1; i++)
+        arr[i] = arr[i + 1];
+
+    printf("Updated array: ");
+    for(int i = 0; i < n - 1; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
