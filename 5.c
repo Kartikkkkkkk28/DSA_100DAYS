629fca05cc0ec658b3359052f348d66cbbdaba98
Kartik Srivastava <kartiksrivastav765@gmail.com>
2026-02-05 14:21:23 +0530

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int p, q;
+
+    // Read size of first log
+    scanf("%d", &p);
+    int log1[p];
+
+    for(int i = 0; i < p; i++)
+        scanf("%d", &log1[i]);
+
+    // Read size of second log
+    scanf("%d", &q);
+    int log2[q];
+
+    for(int i = 0; i < q; i++)
+        scanf("%d", &log2[i]);
+
+    int merged[p + q];
+
+    int i = 0, j = 0, k = 0;
+
+    // Merge both arrays
+    while(i < p && j < q) {
+        if(log1[i] <= log2[j])
+            merged[k++] = log1[i++];
+        else
+            merged[k++] = log2[j++];
+    }
+
+    // Remaining elements
+    while(i < p)
+        merged[k++] = log1[i++];
+
+    while(j < q)
+        merged[k++] = log2[j++];
+
+    // Print result
+    for(int x = 0; x < p + q; x++)
+        printf("%d ", merged[x]);
+
+    return 0;
+}
